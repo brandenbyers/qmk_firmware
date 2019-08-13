f6d3ae4b50f6b94e95f84786cd10a8ef32a70585
Branden Byers <branden@brandenbyers.com>
2019-08-13 15:41:26 -0700

    Convert stenoger to stenocle

---
+++ keyboards/ckeys/stenocle/keymaps/default/keymap.c
@@ -0,0 +1,31 @@
+#include QMK_KEYBOARD_H
+#include "keymap_steno.h"
+
+enum layers {
+  _BASE  // base layer
+};
+
+extern keymap_config_t keymap_config;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /* Plover (http://opensteno.org)
+ * ,----------------------------------------------------------------------------.
+ * |  FN  |   S  |   T  |   P  |   H  |   *  |   F  |   P  |   L  |   T  |   D  |
+ * |------+------+------+------+------+------|------+------+------+------+------|
+ * |  PWR |   S  |   K  |   W  |   R  |   *  |   R  |   B  |   G  |   S  |   Z  |
+ * |------+------+------+------+------+------+------+------+------+------+------`
+ *                 |   #  |   A  |   O  |  |   E  |   U  |   #  |
+ * `                --------------------------------------------
+ */
+
+  [_BASE] = LAYOUT(
+    STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
+    STN_PWR, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
+                           STN_NA,   STN_A,   STN_O,  STN_E,   STN_U,   STN_NB
+  ),
+};
+
+void matrix_init_user() {
+  steno_set_mode(STENO_MODE_GEMINI); // or STENO_MODE_BOLT
+}
